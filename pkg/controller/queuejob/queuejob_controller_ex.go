e391c7087147b19864aaae280e79558057e5659c
Jaroslaw Cwiklik <cwiklik@wecm-9-67-155-218.wecm.ibm.com>
2023-11-30 08:52:28 -0500

    enable setting gvk in ItemCompletionStatus

--- pkg/controller/queuejob/queuejob_controller_ex.go
+++ pkg/controller/queuejob/queuejob_controller_ex.go
@@ -608,19 +608,26 @@ func (qjm *XController) getAppWrapperCompletionStatus(caw *arbv1.AppWrapper) (ar
 					}
 				} 
 			}
-			var gvk = schema.GroupVersionKind{}
+			var gvk = arbv1.ItemGVK{}
 			_, groupversionkind, err := unstructured.UnstructuredJSONScheme.Decode(objectName.Raw, nil, nil)
 			if err != nil || groupversionkind == nil {
 				klog.Errorf("[getAppWrapperCompletionStatus] Error unmarshalling gvk, err=%#v", err)
 				unknown := "Unknown"
-				gvk = schema.GroupVersionKind {
+				gvk = arbv1.ItemGVK{
 					Group:   unknown,
 					Version:  unknown,
 					Kind:     unknown,
 				}
 			}
             if groupversionkind != nil {
-				gvk = *groupversionkind
+				gvk = arbv1.ItemGVK{
+					Group:   groupversionkind.Group,
+					Version:  groupversionkind.Version,
+					Kind:     groupversionkind.Kind,
+				}
+				klog.Infof("[getAppWrapperCompletionStatus] - groupversionkind: %v",gvk)
+			} else {
+				klog.Infof("[getAppWrapperCompletionStatus] - groupversionkind NOT available")
 			}
 
 			if len(name) == 0 {
@@ -635,12 +642,11 @@ func (qjm *XController) getAppWrapperCompletionStatus(caw *arbv1.AppWrapper) (ar
 				return caw.Status.State, completionStatus
 			} else {
 				genItemCompletionStatus := arbv1.GenericItem{
-					GroupVersionKind: schema.GroupVersionKind {
-                        Group:   gvk.Group,
+					Gvk: arbv1.ItemGVK {
+						Group: gvk.Group,
 						Version: gvk.Version,
-						Kind:    gvk.Kind,
+						Kind: gvk.Kind,
 					},
-
 					Name:      name,
 					Namespace: namespace,
 					Condition: condition,
