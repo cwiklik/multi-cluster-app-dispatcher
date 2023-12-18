e36994bffd0a650daea151a0b79167419d2842fa
Jaroslaw Cwiklik <cwiklik@Jaroslaws-MacBook-Pro.local>
2023-12-18 11:13:11 -0500

    insure the PolicyResult in AW is updated once

--- pkg/controller/queuejob/queuejob_controller_ex.go
+++ pkg/controller/queuejob/queuejob_controller_ex.go
@@ -170,7 +170,7 @@ func NewJobController(config *rest.Config, serverOption *options.ServerOption) *
 		cache:           clusterstatecache.New(config),
 		schedulingAW:    nil,
 	}
-	cc.metricsAdapter = adapter.New(serverOption, config, cc.cache)
+		cc.metricsAdapter = adapter.New(serverOption, config, cc.cache)
 
 	cc.genericresources = genericresource.NewAppWrapperGenericResource(config)
 
@@ -2185,7 +2185,9 @@ func (cc *XController) manageQueueJob(ctx context.Context, qj *arbv1.AppWrapper,
 			queuejobKey, _ := GetQueueJobKey(qj)
 			if agentId, ok := cc.dispatchMap[queuejobKey]; ok {
 				klog.V(10).Infof("[Dispatcher Controller] Dispatched AppWrapper %s to Agent ID: %s.", qj.Name, agentId)
-				if cc.serverOption.ExternalDispatch {
+				policyResult := qj.Spec.SchedSpec.ClusterScheduling.PolicyResult
+				// check if policyResult is already set
+				if cc.serverOption.ExternalDispatch && reflect.ValueOf(policyResult).IsZero() {					
 					values := strings.Split(agentId, "/")
 					klog.V(10).Infof("[Dispatcher Controller] Dispatching AppWrapper %s to Agent ID: %s Through External Dispatcher.", qj.Name, values[len(values)-1])					
 					qj.Spec.SchedSpec.ClusterScheduling.PolicyResult = arbv1.ClusterDecision {
